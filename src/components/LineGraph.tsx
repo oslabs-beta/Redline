0ba3bd1f0e26e4d9b0915ddad132ac91ba4060bb
Luke Driscoll <lukedriscoll@Lukes-MacBook-Air.local>
2023-01-05 19:59:02 -0500

    update shape of state, retrieve data, create interfaces

--- src/components/LineGraph.tsx
+++ src/components/LineGraph.tsx
@@ -1,39 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { CategoryScale, Chart, registerables } from "chart.js";
-import { setLabels } from "react-chartjs-2/dist/utils";
+import { CategoryScale, Chart, registerables } from 'chart.js';
+import { setLabels } from 'react-chartjs-2/dist/utils';
 import { Line } from 'react-chartjs-2';
 
 Chart.register(...registerables);
 
 interface lineProps {
-  lineData: number[]
+  lineData: number[];
 }
 
-export default function LineGraph({ lineData } : lineProps): JSX.Element {
+export default function LineGraph({ lineData }: lineProps): JSX.Element {
+  console.log(lineData);
   const labels: number[] = [];
-  lineData.forEach((el,ind) => {
+  lineData.forEach((el, ind) => {
     labels.push(ind); //[1,2,3,4,5,6]
     // console.log(labels);
-  })
+  });
   const data = {
     labels: labels,
-    datasets: [{
-      label: 'hello',
-      data: lineData, //[array of data which are nums]
-      borderColor: 'black',
-      borderWidth: 1,
-      // xAxisID: 'time',
-      // yAxisID: 'y'
-    }]
-  }
+    datasets: [
+      {
+        label: 'hello',
+        data: lineData, //[array of data which are nums]
+        borderColor: 'white',
+        borderWidth: 1,
+        // xAxisID: 'time',
+        // yAxisID: 'y'
+      },
+    ],
+  };
   return (
-    <div style={{'width':'800px'}}>LineGraph
-      <Line data={data}
-    // add chart data labels, datasets, options and properties
+    <div style={{ width: '800px' }}>
+      LineGraph
+      <Line
+        data={data}
+        // add chart data labels, datasets, options and properties
         options={{
           plugins: {
-            title: {display: true, text: "hello world"},
-            legend: {display: false}
+            title: { display: true, text: 'hello world' },
+            legend: { display: false },
           },
           scales: {
             x: {
@@ -42,17 +47,10 @@ export default function LineGraph({ lineData } : lineProps): JSX.Element {
             y: {
               title: { display: true, text: 'Memory (in bytes)' },
               beginAtZero: true,
-
-            }
-          }
+            },
+          },
         }}
       />
-    </div>    
-  )
-
+    </div>
+  );
 }
-
-
-
-
-
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,58 +2,63 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Sidebar from './Sidebar';
 import MetricContainer from './MetricContainer';
+import { MetricCollection } from '../../types/types';
 
 export default function Main() {
   /* Declare state that we pass down to sidebar. Sidebar is where the user is entering the endpoints and where the declared state will be updated.
 
-  const endpoints = {
-  'nickname' : {
-    host: 'host',
-    port: 3000,
-    password: 'helloworld'
-  }
-}
-  const pieData = {
-    memoryUsage: [50, 50],
-    hitRatio: [90,10]
-  }
-
-  When the user clicks on their various endpoints, they'll be able to see those specific metrics because we can make a request to the backend only requesting information from the endpoint associated with that nickname.
-
   */
   const [endpoint, setEndpoint] = useState();
-  const [metrics, setMetrics] = useState();
-  const [pieData, setPieData] = useState('');
-  const [lineData, setLineData] = useState();
-  const [barData, setBarData] = useState();
+  const [metrics, setMetrics] = useState<MetricCollection[]>([]); //store array of metric object instances (that updates every 2s)
+  // const [pieData, setPieData] = useState('');
+  // const [lineData, setLineData] = useState();
+  // const [barData, setBarData] = useState();
 
   // Grab the redis metrics
   useEffect(() => {
-    intervalAction();
-  }, []);
-
-  function intervalAction() {
-    const getData = async () => {
+    setTimeout(async () => {
       try {
-        const response = await axios.post(
+        const response = await axios.get(
           'http://localhost:3000/api/retrieveMetrics'
         );
-        setMetrics(response.data.allData);
-        setPieData(response.data.allData.used_memory_dataset_perc);
+        // console.log(response.data);
+        if (metrics.length == 4) {
+          // <-- add variable set by user potentially
+          setMetrics(() => [...metrics.slice(-3), response.data]); //[{metrics1}, {metrics2}, {metrics3}]
+        } else {
+          setMetrics(() => [...metrics, response.data]);
+        }
+        // console.log(metrics);
+        // setMetrics(response.data.allData);
+        // setPieData(response.data.allData.used_memory_dataset_perc); //setPieData(resp.data);
       } catch (err) {
         console.log(err);
       }
-    };
-    setTimeout(() => {
-      getData();
-      intervalAction();
     }, 2_000);
-  }
+  }, [metrics]);
+
+  // function intervalAction() {
+  //   const getData = async () => {
+  //     try {
+  //       const response = await axios.post(
+  //         'http://localhost:3000/api/retrieveMetrics'
+  //       );
+  //       setMetrics(response.data.allData);
+  //       setPieData(response.data.allData.used_memory_dataset_perc);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   setTimeout(() => {
+  //     getData();
+  //     intervalAction();
+  //   }, 2_000);
+  // }
 
   return (
     <div>
       <Sidebar />
-      <MetricContainer pieData={pieData} lineData={lineData} />
+      <MetricContainer metrics={metrics} />
     </div>
   );
 }
