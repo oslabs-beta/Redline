4e2e0bfdb9033516e452e4e73cd622365c525aaf
sakurakiyama <sakura.bowden@gmail.com>
2023-01-21 09:49:16 -0500

    changing pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
-import Main from '../components/Main'
-
-
-export default function Home() {
+import React from 'react';
+import NavBar from '../../src/components/NavBar'; 
+import Image from 'next/image';
+import Luke from '../public/luke.png'; 
+import Alan from '../public/alan.png'; 
+import Elvin from '../public/elvin.png'; 
+import Sakura from '../public/sakura.png'
+import styles from '../components/styles/Landing.module.scss'
+export default function Landing() {
   return (
-    <Main/>
+    <div>
+      <NavBar />
+    <div className='team'>
+    <Image className={styles.teamPhoto} src={Luke} alt='Luke'/>
+    <Image className={styles.teamPhoto} src={Elvin} alt='Elvin'/>
+    <Image className={styles.teamPhoto} src={Alan} alt='Alan'/>
+    <Image className={styles.teamPhoto} src={Sakura} alt='Sakura'/>
+    </div>
+    </div>
   )
 }
